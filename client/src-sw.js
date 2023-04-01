0d867620d8c2247eb17895d6a0e09c7d6ce821c6
Avery <averycaldwell7@gmail.com>
2023-04-01 16:16:44 -0700

    added bundle and corrected title name

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,16 +27,12 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute(
-    // FIXME - pasted from activities
-    // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
-    ({ request }) =>
-        ['style', 'script', 'worker'].includes(request.destination),
-    // new CasheableResponsePlugin({
-    new offlineFallback({
-        // Name of the cache storage.
-        cacheName: 'asset-cache',
-        plugins: [
+// Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new offlineFallback({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
             // This plugin will cache responses with these headers to a maximum-age of 30 days
             new CacheableResponsePlugin({
                 statuses: [0, 200],
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
     return {
@@ -12,6 +12,9 @@ module.exports = () => {
         entry: {
             main: './src/js/index.js',
             install: './src/js/install.js',
+            database: './src/js/database.js',
+            editor: './src/js/editor.js',
+            header: './src/js/header.js',
         },
         output: {
             filename: '[name].bundle.js',
@@ -22,7 +25,7 @@ module.exports = () => {
             // Webpack plugin that generates our html file and injects our bundles.
             new HtmlWebpackPlugin({
                 template: './index.html',
-                title: 'Another Text Editor PWA',
+                title: 'Just Another Text Editor',
             }),
             // //FIXME - added inject
             // new MiniCssExtractPlugin(),
@@ -35,10 +38,9 @@ module.exports = () => {
             new WebpackPwaManifest({
                 fingerprints: false,
                 inject: true,
-                name: 'Another Text Editor PWA',
-                short_name: 'Text Editor',
-                description:
-                    'A text editor single-page application that runs in the browser, meets the PWA criteria, and features a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser',
+                name: 'Just Another Text Editor',
+                short_name: 'JATE',
+                description: 'Just Another Text Editor',
                 background_color: '#225ca3',
                 theme_color: '#225ca3',
                 start_url: './',
@@ -53,8 +55,9 @@ module.exports = () => {
             }),
         ],
 
-        // FIXME - added rules
+        // Added rules
         module: {
+            // CSS loaders
             rules: [
                 {
                     test: /\.css$/i,
@@ -63,6 +66,7 @@ module.exports = () => {
                 {
                     test: /\.m?js$/,
                     exclude: /node_modules/,
+                    // Added babel to webpack.
                     use: {
                         loader: 'babel-loader',
                         options: {
